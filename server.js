80fe5f0b91c8584d3f3481481b018dbce8b290cb
AndrewWigginCout <91389007+AndrewWigginCout@users.noreply.github.com>
2022-12-24 17:00:55 -0800

    simp

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ app.post('/', (req,res)=>{
   req.body.date=new Date()
   var car = req.body.prefer && req.body.prefer.includes("car")
   var json = JSON.stringify(req.body,null,2)
-  var rs = "POST RECIEVED<br/>"
+  var rs = `<h3>Thanks for your interest simp.</h3><br/>
+           I'll add you to the pile of applications. Maybe I'll contact you if I get bored.<br>`
   if (car){
     rs+="FYI my favorite muscle car is the Ford Mustang. ;)<br>"
     rs+='<img src="mustang.webp"><br>'
   }
   res.status(200).send(rs+"<pre>"+json+"</pre")
 })
-app.use(express.static('.'))
+app.use(express.static('www'))
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send(`<h1>404</h1>`)
